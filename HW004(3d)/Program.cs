a77892783f6fa1b61a842e00cb28fa5bde26f361
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-21 18:36:57 +0500

    perhaps a working program has been created

---
+++ HW004(3d)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//Найти расстояние между точками в пространстве 3D
+double x1 = new Random().Next(1, 20);
+double y1 = new Random().Next(1, 20);
+double x2 = new Random().Next(1, 20);
+double y2 = new Random().Next(1, 20);
+double z1 = new Random().Next(1, 20);
+double z2 = new Random().Next(1, 20);
+double stepen = 2;
+
+double diffx = x1-x2;
+double diffy = y1-y2;
+double diffz = z1-z2;
+double dist = Math.Sqrt((Math.Pow(diffx, stepen) + Math.Pow(diffy, stepen) + Math.Pow(diffz, stepen)));
+
+Console.Write("Расстояние между точками: " + dist);
--- HW004/Program.cs
+++ HW004/Program.cs
@@ -1,1 +1,12 @@
-﻿//Найти расстояние между точками в пространстве 2D/3D
+﻿//Найти расстояние между точками в пространстве 2D
+double x1 = new Random().Next(1, 20);
+double y1 = new Random().Next(1, 20);
+double x2 = new Random().Next(1, 20);
+double y2 = new Random().Next(1, 20);
+double stepen = 2;
+
+double diffx = x1-x2;
+double diffy = y1-y2;
+double dist = Math.Sqrt((Math.Pow(diffx, stepen) + Math.Pow(diffy, stepen)));
+
+Console.Write("Расстояние между точками: " + dist);
