fb83cea88d6ab3e8df17357f8a89ab1f3017bddc
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-21 10:27:07 +0500

    perhaps a working program has been created

--- HW002/Program.cs
+++ HW002/Program.cs
@@ -1,1 +1,14 @@
 ﻿//По двум заданным числам проверять является ли одно квадратом другого
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine() ??"0");
+
+if ((num2*num2)==num1)
+{
+    Console.Write("Число " + num1 + " является квадратом числа " + num2);
+}
+else
+{
+    Console.Write("Число " + num1 + " НЕ является квадратом числа " + num2);
+}
